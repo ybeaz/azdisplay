e4396d6b985b4bc3811db0ec494af228c90215b9
ybeaz <t3531350@yahoo.com>
2019-03-01 13:13:26 -0800

    Add new files 2

---
+++ hp2p_v01/src/ViewLayer/Components/CombineWrapper.react.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CombineWrapper = props => {
+  const { classStyle, children } = props
+  return (
+    <div className={`container-fluid ${classStyle}`}>
+      <div className='row'>
+        <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+CombineWrapper.propTypes = {
+}
+
+export default CombineWrapper
---
+++ hp2p_v01/src/ViewLayer/Components/NavBar.react.js
@@ -0,0 +1,51 @@
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// eslint-disable-next-line react/prefer-stateless-function
+class NavBar extends React.PureComponent {
+
+
+  render() {
+    const { propsScope } = this.props
+    const { sid, login } = propsScope
+    // console.info('NavBar->render() [10]',{});
+    return (
+      <div id={sid} className={sid}>
+        <nav id='navbarNav' className='navbar navbar-expand-sm'>
+          <a className='navbar-brand navbarBrend' href='#navbar'>
+            <div className='logoNav iconLogo' />
+          </a>
+          {/*
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>link 1</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>Link 2</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>Link 3</a>
+            </li>
+          </ul>
+          */}
+        </nav>
+
+        <ul className='nav-actions'>
+          <li className='nav-item has-auth-popup'>
+            <a className='nav-link' href='#'>{login}</a>
+          </li>
+          <li className='nav-item has-lang-popup'>
+            <a className='nav-link' href='#'>Язык</a>
+          </li>
+        </ul>
+
+      </div>
+    )
+  }
+}
+
+NavBar.propTypes = {
+}
+
+export default NavBar
